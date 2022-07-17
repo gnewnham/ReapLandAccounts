4269a47627c1e4e6ef63fbcff1983f53afffa9ae
gnewnham <38093775+gnewnham@users.noreply.github.com>
2022-07-17 13:50:34 +1000

    Create functions.py
    added a dummy python file for a class containing REAP related functions

---
+++ functions.py
@@ -0,0 +1,10 @@
+
+class ReapFunctions:
+	'''
+	DocString: This is a place holder for REAP functions.
+  I just wanted to tell GitHub that this is a python project 
+	'''
+	
+	def __init__(self, infile, instParams=defaultInstParams):
+    print("this is the REAP repository")
+    
